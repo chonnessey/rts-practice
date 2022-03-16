3228ef23675ff217f3d145a5e5284bf4f7e9ce4c
Adrian Camacho <adriancamacho18@gmail.com>
2022-03-15 19:22:34 -0700

    add guestlist component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import ReactDom from 'react-dom';
+import GuestList from './state/GuestList'
 
 const App = () => {
   return (
     <div>
-      <h1>Hi There!</h1>
+      <GuestList />
     </div>
   );
 };
