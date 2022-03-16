32a18ceabd802cab9ff123f5c598ddf22748eca3
Adrian Camacho <adriancamacho18@gmail.com>
2022-03-16 12:32:36 -0700

    add refs folder and update copied usersearch component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import ReactDom from 'react-dom';
 // import GuestList from './state/GuestList'
 // import UserSearch from './state/UserSearch';
-import EventComponent from './events/EventComponent'
+// import EventComponent from './events/EventComponent'
+import UserSearch from './refs/UserSearch'
 
 const App = () => {
   return (
     <div>
       {/* <GuestList /> */}
-      {/* <UserSearch /> */}
-      <EventComponent />
+      <UserSearch />
+      {/* <EventComponent /> */}
     </div>
   );
 };
---
+++ src/refs/UserSearch.tsx
@@ -0,0 +1,52 @@
+import { useState, useRef, useEffect } from 'react';
+
+const users = [
+  { name: 'Sarah', age: 34 },
+  { name: 'Ab', age: 34 },
+  { name: 'Brother', age: 34 },
+  { name: 'Moo', age: 34 }
+];
+
+const UserSearch: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [name, setName] = useState('');
+  const [user, setUser] = useState<{ name: string, age: number} | undefined>()
+
+  useEffect(() => {
+    if(!inputRef.current) {
+      return;
+    }
+    inputRef.current.focus()
+  }, [])
+  
+  const handleClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    })
+
+    setUser(foundUser)
+  };
+
+  return (
+    <div>
+      <input 
+        ref={inputRef}
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <br/>
+      <br/>
+      <button onClick={handleClick}>Find user</button>
+      <br/>
+      <br/>
+      <div>
+        name: {user && user.name}
+        <br/>
+        <br/>
+        age: {user && user.age}
+      </div>
+    </div>
+  )
+};
+
+export default UserSearch;
